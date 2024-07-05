6c8ea5b375b4d94d8d388034e10f42cec5e065fe
Alan <Alan2003@sju.ac.kr>
2024-07-05 15:07:29 +0900

    parseInt,parseFloat,isNaN,isFinite

--- script.js
+++ script.js
@@ -164,7 +164,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -182,7 +182,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = +inputLoanAmount.value;
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -223,7 +223,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -251,3 +251,30 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+console.log(23 == 23.0);
+console.log(0.1 + 0.2 == 0.3); // false because of javascript as we can do financial calculation with javaScript like any other languages like php
+
+// converting strings into numbers ==========================+>
+console.log(Number('23'));
+console.log(+'23'); // this is same as above but with cleaner way
+// 1)
+//pasing
+console.log(Number.parseInt('33px'));
+console.log(Number.parseInt('2.3rem'));
+console.log(Number.parseFloat('2.3rem')); // use with css to read number out of string
+//
+//2)
+//check if value is NaN which returns boolean true/false
+console.log(Number.isNaN(23)); // false
+console.log(Number.isNaN('23')); // false
+console.log(Number.isNaN('23px')); // false
+console.log(isNaN('hello')); // true
+console.log(Number.isNaN('hello')); // false
+console.log(Number.isNaN(+'23px')); // true
+//
+//3)
+// check if value is number . returns boolean true/false
+console.log(Number.isFinite(23)); // true
+console.log(Number.isFinite('23')); // false
+console.log(Number.isFinite('34px')); // false
+console.log(Number.isFinite(+'m23')); // false
