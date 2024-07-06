33bcd95c7be264423369dd8b3d41bee2851d0182
Alan <Alan2003@sju.ac.kr>
2024-07-07 00:01:01 +0900

    big iNt

--- script.js
+++ script.js
@@ -338,3 +338,9 @@ labelBalance.addEventListener('click', function () {
     }
   });
 });
+/// big ints
+console.log(23112121212121223432423423432432432423423); // output 2.311212121212121e+39 because the biggest number in javaScript is
+console.log(2 ** 53 - 1); // biggest num in javaScript 9007199254740991
+// however when we work with API's we will have to deal with so many numbers like above so in ES20 'bigint' method cameout
+
+console.log(23112121212121223432423423432432432423423n); //23112121212121223432423423432432432423423n this is the converted to string using n which is big_int. we can use it when we deal with API
